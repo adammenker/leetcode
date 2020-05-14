78ac6358c87bc85d77e3ff42ed254e34ff90e2f4
Adam <adam.j.menker@gmail.com>
2020-05-14 16:40:18 -0500

    contains duplicate solution

---
+++ js/contains-duplicate.js
@@ -0,0 +1,22 @@
+// check to see if there are duplicates in array of integers
+
+// i think my time complexity is O(2n log(n)), sort takes n log(n) and then i do another O(n) loop to check
+// time: 31%
+// space: 88%
+
+var containsDuplicate = function(nums) {
+    if(nums){
+        if(nums.length === 1){
+            return false;
+        }
+        nums = nums.sort();
+        for(let i = 0; i < nums.length - 1; i++){
+            if(nums[i] === nums[i + 1]) return true;
+        }
+        return false;
+    }
+};
+
+check = [1,3,4,2,5,7,8,33,44,55,9,1];
+
+console.log(containsDuplicate(check));
